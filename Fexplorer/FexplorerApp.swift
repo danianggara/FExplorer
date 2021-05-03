d98ee3e903e4521f858fb1912065e342cf1ff54c
Dani Anggara <danyanggara149@gmail.com>
2021-05-03 08:06:27 +0700

    Initial Commit

---
+++ Fexplorer/FexplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FexplorerApp.swift
+//  Fexplorer
+//
+//  Created by Dani Anggara on 03/05/21.
+//
+
+import SwiftUI
+
+@main
+struct FexplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
